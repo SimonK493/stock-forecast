1c6be11c81e1b17eec4bd8fe35d3ef1ee219d76c
SimonK493 <simon.kornacker@gmail.com>
2024-05-19 00:11:57 +0200

    Added TODO

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,6 +1,19 @@
 import sklearn
 import pandas as pd
-
+""" 
+ TODO: 
+    - Fehlende Daten ersetzen oder entfernen
+    - Unregelmäßige Handelszeiten behandeln (--> evtl. wird nicht jeden Tag gehandelt)
+    - Ausreiser identifizieren und handhaben
+    - Datumsformat kontrollieren
+    - Anpassen der Splits und Dividenden (?)
+    - Skalieren der Daten, z.B. Min-Max-Skalierung
+    - Feature Engineering, z.B. Gleitende Durschnitte über mehrere Tage
+"""
 def cleaner(data):
     df = data
 
+
+
+if __name__ == "__main__":
+    cleaner()
--- downloader.py
+++ downloader.py
@@ -4,7 +4,8 @@ import yfinance as yf
 
 def data_downloader():
     # "Most interesting" shares in the sectors: Technology, Financials, Healthcare, Consumer Discretionary, Industrials, Energy, Utilities
-    tickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "NVDA", "JPM", "GS", "BAC", "JNJ", "PFE", "MRK", "KO", "PEP", "PG", "GE", "BA", "CAT", "XOM", "CVX", "COP", "DUK", "NEE", "D"]
+    #tickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "NVDA", "JPM", "GS", "BAC", "JNJ", "PFE", "MRK", "KO", "PEP", "PG", "GE", "BA", "CAT", "XOM", "CVX", "COP", "DUK", "NEE", "D"]
+    tickers = ["AAPL"]
     data = yf.download(tickers,period = "1d")
     return data
 
